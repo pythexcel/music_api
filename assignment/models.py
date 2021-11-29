716463fb415db162d802911ef4187b2da7316278
pythexcel <pythexcel@excellencetechnologies.in>
2021-11-29 12:39:24 +0530

    fields updated.

--- assignment/models.py
+++ assignment/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Assignment(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    music_genre = models.CharField(max_length=50)
-    daily_practice_time = models.IntegerField()
+    musicGenre = models.CharField(max_length=50)
+    dailyPracticeTime = models.IntegerField()
     days = models.IntegerField()
-    days_practiced = models.IntegerField()
+    daysPracticed = models.IntegerField()
 
     def __str__(self):
         return self.title
