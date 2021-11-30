6aa4156e2fd89e4aa98fd2407b5390d7889deaec
pythexcel <pythexcel@excellencetechnologies.in>
2021-11-30 17:10:36 +0530

    updated serializers

--- assignment/serializers.py
+++ assignment/serializers.py
@@ -1,9 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Assignment
 
 class AssignmentSerializer(ModelSerializer):
+    musicGenre = SerializerMethodField()
+    dailyPracticeTime = SerializerMethodField()
+    daysPracticed = SerializerMethodField()
+
     class Meta:
         model = Assignment
-        fields = '__all__'
+        fields = ['title', 'description', 'musicGenre', 'dailyPracticeTime', 'days', 'daysPracticed']
 
-    
+    def get_musicGenre(self, obj):
+        return obj.music_genre
+
+    def get_dailyPracticeTime(self, obj):
+        return obj.daily_practice_time
+
+    def get_daysPracticed(self, obj):
+        return obj.days_practiced
