df2d7d4728adb427c221e72c4d67b38395de4bdd
pythexcel <pythexcel@excellencetechnologies.in>
2021-12-01 11:53:21 +0530

    updation done

--- assignment/views.py
+++ assignment/views.py
@@ -1,18 +1,18 @@
 from rest_framework.views import APIView
 from assignment.models import Assignment
-from .serializers import AssignmentGetSerializer, AssignmentPostSerializer
+from .serializers import AssignmentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
 class AssignmentView(APIView):
     def get(self, request):
         obj = Assignment.objects.all()
-        serializer = AssignmentGetSerializer(obj, many=True)
+        serializer = AssignmentSerializer(obj, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
     def post(self, request):
-        serializer = AssignmentPostSerializer(data=request.data)
+        serializer = AssignmentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
